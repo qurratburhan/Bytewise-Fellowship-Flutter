8dcccd9285cbd0f7c780299f21f2a98ccb01457d
qurratburhan <164742805+qurratburhan@users.noreply.github.com>
2024-06-18 23:48:16 +0500

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 3;
+  double b = 5.2566;
+  String fruit = 'Apple';
+  bool it = true;
+  List<int> scores = [85, 90, 78, 56, 466];
+  Map<String, String> address = {'city': 'New York', 'country': 'USA'};
+
+  print('Fruit: $fruit');
+  print('Value: $a');
+  print('Float: $b');
+  print('Is Active: $it');
+  print('Scores: $scores');
+  print('$address');
+}
